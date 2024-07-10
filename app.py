eb6a4f5e9f86ad57c919b72561b435520de39012
Esteban Maya Cadavid <emayacadavid9@gmail.com>
2024-07-10 16:27:08 -0500

    🚀 Setup initial project example

---
+++ app.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from config import settings
+
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    secret_name: str
+    age: int | None = Field(default=None, index=True)
+
+
+engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.get("/")
+def hello():
+    return "Hello, Docker!"
+
+
+@app.post("/heroes/")
+def create_hero(hero: Hero):
+    with Session(engine) as session:
+        session.add(hero)
+        session.commit()
+        session.refresh(hero)
+        return hero
+
+
+@app.get("/heroes/")
+def read_heroes():
+    with Session(engine) as session:
+        heroes = session.exec(select(Hero)).all()
+        return heroes
